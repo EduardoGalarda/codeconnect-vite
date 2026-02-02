3edbc1d5f3ce10941c66b92aad2630d5a0ea722a
EduardoGalarda <eduardo.galarda@yahoo.com>
2026-02-02 00:20:54 -0500

    chore: Remove multiple image assets and update App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
 
 const PageLogin = lazy(() => import('./Pages/PageLogin'))
 
